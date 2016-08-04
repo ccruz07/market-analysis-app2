933cc7c1027b7c2995dd2ab4ceeaaa45e7842262
ccruz07 <cruz1706@gmail.com>
2016-08-03 19:18:18 -0700

    Bus Mall 1st draft

---
+++ CLS10BMapp.js
@@ -0,0 +1,16 @@
+//CLS10-BMapp.js
+var imgTrckr = function (name, source){
+
+
+  this.name = name;
+  this.imgSource = source;
+};
+
+var imgUpVotes = 0;
+var totalUpVotes = 0;
+
+var bmImgOpt = [
+   new imgTrckr('Cthulhu', 'img/cthulhu.jpg'),
+   new imgTrckr('wine_glass', 'img/wine_glass.jpg'),
+
+]
